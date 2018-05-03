40241075f025c7b35148dae1ca6cb58320d44441
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-05-03 03:34:48 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -4,33 +4,11 @@ class Book
  end
  
  attr_accessor :author, :page_count, :genre
+ 
  def title
    @title
  end
  
- def author=(author)
-   @author = author
- end
- 
- def author
-   @author
- end
- 
- def page_count=(num)
-   @page_count= num
- end
- 
- def page_count
-   @page_count
- end
- 
- def genre=(genre)
-   @genre = genre
- end
- 
- def genre
-   @genre
- end
  
  def turn_page
    puts "Flipping the page...wow, you read fast!"
